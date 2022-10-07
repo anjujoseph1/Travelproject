c57cb15fcfa90987af06d4b3ff9fc7535f00a93b
Anju Joseph <anjuzz955@gmail.com>
2022-10-07 15:42:52 +0530

    first

---
+++ travelapp1/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class place(models.Model):
+    name=models.CharField(max_length=100)
+    img=models.ImageField(upload_to='picture')
+    desc=models.TextField()
+    price=models.IntegerField()
+    offer=models.BooleanField(default=False)
---
+++ travelapp1/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from.import views
+urlpatterns=[
+    path('',views.fun,name='fun'),
+    path('add',views.addition,name='add')
+]
